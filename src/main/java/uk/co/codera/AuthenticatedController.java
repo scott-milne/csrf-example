f72cca4b2bb278146bb9e1b58ad5aa5318499c82
Scott Milne <scott.milne@codera.co.uk>
2016-12-13 22:48:28 +0000

    Adding working 'bank' website

--- src/main/java/uk/co/codera/AuthenticatedController.java
+++ src/main/java/uk/co/codera/AuthenticatedController.java
@@ -1,5 +1,6 @@
 package uk.co.codera;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticatedController {
 
-    @RequestMapping(path = "/deposit", method = RequestMethod.GET)
-    public String deposit() {
-        return "Greeting from Spring Boot!";
+    private double balance = 10000.0;
+
+    @RequestMapping(path = "/balance", method = RequestMethod.GET)
+    public String balance() {
+        return "Your balance is £" + String.format("%.02f", this.balance);
+    }
+
+    @RequestMapping(path = "/withdraw/{amount}", method = RequestMethod.GET)
+    public String withdraw(@PathVariable("amount") double amount) {
+        this.balance -= amount;
+        return balance();
     }
 }
--- src/test/java/uk/co/codera/AuthenticatedControlllerIT.java
+++ src/test/java/uk/co/codera/AuthenticatedControlllerIT.java
@@ -4,12 +4,31 @@ import static com.jayway.restassured.RestAssured.get;
 import static org.hamcrest.CoreMatchers.containsString;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CsrfExampleApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class AuthenticatedControlllerIT {
 
+    private final double startingBalance = 10000.00;
+
     @Test
-    public void test() {
-        get("/deposit").then().assertThat().body(containsString("Greeting from Spring Boot!"));
+    public void shouldDisplayBalance() {
+        get("/balance").then().assertThat()
+                .body(containsString("Your balance is £" + String.format("%.02f", this.startingBalance)));
+    }
+
+    @Test
+    public void shouldWithdrawAmount() {
+        final int amount = 20;
+        get("/withdraw/" + amount).then().assertThat()
+                .body(containsString("Your balance is £" + String.format("%.02f", this.startingBalance - amount)));
     }
 
 }
